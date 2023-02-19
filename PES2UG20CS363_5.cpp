b35019d48e2fe70cf85b111e0456213b9eb3e137
SwethaSathyamoorthy <118299130+SwethaSathyamoorthy@users.noreply.github.com>
2023-02-19 09:59:44 +0530

    Add files via upload

---
+++ PES2UG20CS363_5.cpp
@@ -0,0 +1,12 @@
+//File: pes2ug20cs363_5.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG20CS363\n";
+  cout << "Successful Pipeline\n";
+  cout << "Bye\n";
+  return 0;
+}
